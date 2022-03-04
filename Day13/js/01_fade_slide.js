19abcaaa4ff2f70362d81a8fbfaf85b445e43c38
[msms603179] <[sgudfhrs@naver.com]>
2022-03-04 21:38:51 +0900

    13일차

--- Day13/js/01_fade_slide.js
+++ Day13/js/01_fade_slide.js
@@ -1,15 +1,15 @@
 
 // 문서 준비 이벤트
-$(function (){
+$(function () {
 
-    /*
-        *index : 0부터 시작
+    /* 
+        * index : 0부터 시작 
 
         :eq(index)
-        - 여러 개의 요소들 중에서 n 번째 요소를 선택
+        - 여러 개의 요소들 중에서 index 번째 요소를 선택
 
         :not('선택자')
-        - 여러 개의 쇼소들 중에서 선택한 요소를 제외하여 선택
+        - 여러 개의 요소들 중에서 선택한 요소를 제외하여 선택
 
         :lt(index)
         - index 번째 요소 이전의 요소들을 선택
@@ -39,37 +39,36 @@ $(function (){
         선택요소.appendTo('부모 선택자')
         - 선택한 요소를 부모 선택자의 마지막 자식 요소로 추가
 
-        .befor()
+        .before()
         - 선택한 요소의 앞에 내용을 추가
         .after()
         - 선택한 요소의 뒤에 내용을 추가
         .prepend()
-        - 선택한 요소의 자식 요소 첫 번째 자식요소로 추가
-
-
+        - 선택한 요소의 첫 번째 자식요소로 추가
+        
     */
 
-    // 0.첫번째 이미지를 제외한 나머지 이미지를 숨김
-    //   (첫번째 이후의 요소들을 선택) - hide()
+    // 0. 첫번째 이미지를 제외한 나머지 이미지를 숨김
+    //    (첫번째 이후의 요소들을 선택) - hide()
     $('#slide > div:gt(0)').hide()
-    
-    setInterval( function(){
-    // 1. 5초(5000s) 마다 현재 사진을 fadeOut   
-    // 2.                 다음 사진을 fadeIn
-    // 3.                 현재 사진을 마지막 요소로 추가 
-    
-    // 슬라이드 박스의 첫 번째 자식요소
-    $('#slide > div:first').fadeOut(2000)
 
-    $('#slide > div:first').next().fadeIn(2000)
+    setInterval( function() {
+        // 1. 5초 마다, 현재 사진을 fadeOut
+        // 2.           다음 사진을 fadeIn
+        // 3.           현재 사진을 마지막 요소로 추가
 
-    $('#slide > div:first').appendTo('#slide')
+        // 슬라이드 박스의 첫 번째 자식요소         
+        $('#slide > div:first').fadeOut(2000)
+
+        $('#slide > div:first').next().fadeIn(2000)
+        
+        $('#slide > div:first').appendTo('#slide')
 
     }, 5000)
-    
+
 })
 
-// .fadeOut(2000)       : 2000ms 동안 서서히 사라지게 하는 메소드
-// .next()              : 선택한 요소의 다음 요소를 선택하는 메소드
-// .fadeIn(2000)        : 2000ms 동안 서서히 나타나게 하는 메소드
-// A.appendTo(B)        : A를 부모요소(B)의 마지막 요소로 추가하는 메소드
+// .fadeOut(2000)   : 2000ms 동안 서서히 사라지게 하는 메소드
+// .next()          : 선택한 요소의 다음 요소를 선택하는 메소드
+// .fadeIn(2000)    : 2000ms 동안 서서히 나타나게 하는 메소드
+// A.appendTo(B)    : A 를  부모요소(B)의 마지막 요소로 추가하는 메소드
